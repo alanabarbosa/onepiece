772f9cc6451c817b039d6d5c661594b2fbec3676
alanabarbosa <itsalanayall@hotmail.com>
2022-05-19 19:23:57 -0300

    refatora projeto

--- js/onepiece.js
+++ js/onepiece.js
@@ -1,47 +1,13 @@
-       const animar = $('#animar')
-        const btns = $('.btn')
-        const output = $('#output')
+const buttons = document.querySelectorAll('#buttons button')
+const content = document.querySelectorAll('#content section')
 
-        
-        btns.eq(0).on('click', function(){
-            animar.css('color', 'red').css('backgroundColor', 'white').text('Que pena, você errou! Quem come a Hana Hana no Mi ganha o poder de replicar as partes de seu corpo por qualquer lugar, posibilitando seu usuário de lutar contra vários oponentes no mesmo tempo a partir de seus menbros replicados.').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })
+const activeTab = (index) => {
+    content.forEach((item) => {
+        item.classList.remove('active')
+    })
+  content[index].classList.add('active')
+}
 
-          btns.eq(1).on('click', function(){
-            animar.css('color', 'red').css('backgroundColor', 'white').text('Tente novamente! A Yomi Yomi no Mi permite que seu usário volte à vida depois que morre, seu nome é derivado de Yomigaeru, que significa "Ressurreição".').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })
-
-           btns.eq(2).on('click', function(){
-            animar.css('color', 'red').css('backgroundColor', 'white').text('Errou novamente, Não leu o mangá One Piece mesmo, né?! A Bara Bara no Mi dá o poder de dividir seu corpo, além de tornar o seu usuário mais inume aos ataques recebidos com armas cortantes. Além de separar seu corpo, quem come essa fruta pode controlar as partes de seu corpo, fazendo com que ela se mova até onde seu usuário desejar.').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })         
-
-          btns.eq(3).on('click', function(){
-            animar.css('color', 'red').css('backgroundColor', 'white').text('Acho que alguém aqui precisa ler os mangá de One Piece! A Hito Hito no Mi fi da a qualquer animal personalidade humana, mantendo suas características fisicas de animal, apenas alterando seus pensamentos e atitudes igual a de um humano.').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })
-      
-           btns.eq(4).on('click', function(){
-            animar.css('color', 'green').css('backgroundColor', 'white').text('Parabéns, você acertou em cheio! A Gomu Gomu no Mi foi a fruta que Luffy comeu por acidente quando ainda era jovem, fazendo com que seu corpo ganhasse propriedades de borracha, que junto de muito treino lhe propocionou várias vantagens e defensas sobre seus inimigos').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })
-
-          btns.eq(5).on('click', function(){
-            animar.css('color', 'red').css('backgroundColor', 'white').text('Tente novamente! Esta Akuma no Mi dá o poder ao seu usuário a possibilidade de produzir fios extremamente finos, praticamente invisiveis ao olho nu, porém forte o bastante para controlar todos aqueles que estejam presos a eles. No mangá, Donquixote Doflamingo usou seu poder para prender as nuvens o possibilidade até mesmo de voar.').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })
-
-          btns.eq(6).on('click', function(){
-            animar.css('color', 'red').css('backgroundColor', 'white').text('Não foi dessa vez! Esta Akuma no Mi dá ao seu usuário o poder da invisibilidade, e não é  apenas quem come pode ficar invisível, e sim qualquer objetivo, como por exemplos: navios. Embora a fruta não der força e habilidades, a vantagem de ficar invisivel é muito útil para combates em ataques e defesas.').slideDown('slow', function(){
-                $(this).text('')
-            })
-        })
-
+buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => activeTab(index))
+})
